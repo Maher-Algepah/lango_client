9be674da243881eec24ba619171c9e39e046a16a
Maher <YOUR_EMAIL_ADDRESS>
2025-05-19 14:23:40 -0700

    implement login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <Home />
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
